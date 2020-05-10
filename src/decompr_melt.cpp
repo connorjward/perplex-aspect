1f080ae6aa2fa2af304b41eb6918c78ab35fe4dd
Connor Ward <cward.contrib@fastmail.com>
2020-05-10 17:05:00 +0100

    Returns some manner of composition information now.

--- src/decompr_melt.cpp
+++ src/decompr_melt.cpp
@@ -10,7 +10,7 @@
 #define MAX_ITER 5
 #define RTOL 0.00001
 
-double calc_dT(const double, const double, const double);
+double calc_dT(MeemumWrapper&, const double, const double, const double);
 
 int main(int argc, char* argv[]) {
     const char *project_name { argv[1] };
@@ -21,78 +21,76 @@ int main(int argc, char* argv[]) {
 
     const double dp = (pN - p0) / (n_steps - 1);
 
+    MeemumWrapper g_meemum { project_name };
+
     std::ofstream myfile;
     myfile.open("mydata.csv");
-    myfile << "p,T,S" << std::endl;
+    myfile << "p,T,S";
 
-    MeemumWrapper g_meemum { project_name };
+    std::vector<char*> models = g_meemum.solution_models();
+    for (char* model : models) {
+	myfile << "," << model;
+    }
+
+    myfile << std::endl;
+
     double T = T0;
     double p = p0;
     
-    std::vector<char*> models = g_meemum.solution_models();
-    for (size_t i = 0; i < models.size(); i++) {
-	std::cout << models[i] << std::endl;
-    }
-    exit(0);
+    MinimizeResult *res;
 
     for (p = p0 ; p < pN; p += dp) {
 	// save composition information
-        MinimizeResult *res = g_meemum.minimize(T, p);
+        res = g_meemum.minimize(T, p);
 
-
-	double dT = calc_dT(T, p, dp);
-	T +=dT;
-
-	// memory cleanup
-	delete res;
-    }
-    // save composition information
-        MinimizeResult* res = g_meemum.minimize(T, p);
 	myfile << p << "," << T << "," 
 	    << res->entropy << std::endl;
 
+	double dT = calc_dT(g_meemum, T, p, dp);
+	T +=dT;
+
+    }
+    // save composition information
+        res = g_meemum.minimize(T, p);
+	myfile << p << "," << T << "," 
+	    << res->entropy << std::endl;
+
+	// memory cleanup
+	delete res;
     myfile.close();
 }
 
-//void write(const double T, const double p, const MinimizeResult* res, std::ofstream f) {
-//    myfile 
-//	<< p << "," 
-//	<< T << "," 
-//	<< res->entropy << "," 
-//	<< res->
-//}
+double calc_dT(MeemumWrapper& g_meemum, const double T0, const double p0, const double dp) {
+    MinimizeResult* res = g_meemum.minimize(T0, p0);
+    const double S0 = res->entropy;
 
-double calc_dT(const double T0, const double p0, const double dp) {
-//    g_meemum.minimize(T0, p0);
-//    const double S0 = g_meemum.entropy();
-//
-//    double dT1 = 0;
-//
-//    double alpha = g_meemum.expansivity();
-//    double Cp = g_meemum.heat_capacity();
-//    double rho = g_meemum.density();
-//
-//    double dT2 = -alpha * T0 * dp *1e5 / Cp / rho;
-//
-//    g_meemum.minimize(T0+dT1, p0+dp);
-//    double dS1 = g_meemum.entropy() - S0;
-//
-//    for (size_t i = 0; i < MAX_ITER; i++) {
-//	g_meemum.minimize(T0+dT2, p0+dp);
-//	double dS2 = g_meemum.entropy() - S0;
-//
-//	if (fabs(dS2 / S0) < RTOL)
-//	    return dT2;
-//
-//	double dT3 = (dT1*dS2 - dT2*dS1) / (dS2 - dS1);
-//
-//
-//	// update values for next iteration
-//	dT1 = dT2;
-//	dT2 = dT3;
-//
-//	dS1 = dS2;
-//    }
-//    throw;
+    double dT1 = 0;
+
+    double alpha = res->expansivity;
+    double Cp = res->heat_capacity;
+    double rho = res->density;
+
+    double dT2 = -alpha * T0 * dp *1e5 / Cp / rho;
+
+    res = g_meemum.minimize(T0+dT1, p0+dp);
+    double dS1 = res->entropy - S0;
+
+    for (size_t i = 0; i < MAX_ITER; i++) {
+	res = g_meemum.minimize(T0+dT2, p0+dp);
+	double dS2 = res->entropy - S0;
+
+	if (fabs(dS2 / S0) < RTOL)
+	    return dT2;
+
+	double dT3 = (dT1*dS2 - dT2*dS1) / (dS2 - dS1);
+
+
+	// update values for next iteration
+	dT1 = dT2;
+	dT2 = dT3;
+
+	dS1 = dS2;
+    }
+    throw;
 }
 
